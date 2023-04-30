d1c9ea7751c62886bd730656d30e5ce45ce413c6
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-04-30 23:56:48 +0700

    (-- )

--- Program.cs
+++ Program.cs
@@ -38,38 +38,5 @@ class Program
         {
             Console.WriteLine("Unreachable");
         }
-
-        /*
-        LinkedList<int> nodeValueList = g.GetAllNode();
-        for(int a=0; a<nodeValueList.GetLength(); a++)
-        {
-            Console.WriteLine(nodeValueList.Get(a));
-        }
-
-        
-        g.AddNode("A");
-        g.AddNode("B");
-        g.AddNode("C");
-        g.InsertNode(2, "D");
-        g.AddEdge(0, 1, 1);
-        g.AddEdge(0, 3, 1);
-        g.AddEdge(1, 2, 1);
-        g.AddEdge(3, 2, 1);
-        g.RemoveNode(1);
-
-
-        Console.WriteLine("Part 1: ");
-        for(int i=0; i<g.GetNodeCount(); i++)
-        {
-            Console.WriteLine(g.GetNode(i));
-        }
-
-        Console.WriteLine("Part 2: ");
-        LinkedList<string> nodeValueList = g.GetAllNode();
-        for(int i=0; i<nodeValueList.GetLength(); i++)
-        {
-            Console.WriteLine(nodeValueList.Get(i));
-        }
-        */
     }
 }
